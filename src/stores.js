2e2221ca1787c37beeee613bb57e071baab9a28b
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-19 11:23:36 +0530

    added jeff-bezos profile

--- src/stores.js
+++ src/stores.js
@@ -2,7 +2,8 @@ import { writable } from 'svelte/store';
 
 
 export const usersNames = writable([{ value: 'Krishna Kiran', label: 'krishna', id: 1 },
-{ value: 'Elon Musk', label: 'elon', id:2 }
+{ value: 'Elon Musk', label: 'elon', id:2 },
+{ value: 'Jeff Bezos', label: 'bezos', id:3 },
 ]);
 
 // export const userName = writable('Krishna Kiran');

71f9a868503a6354e26557ae2851f1c9618765ff
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-19 19:14:03 +0530

    implemented data fetching from mongoDB database

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,10 @@
-export async function load({fetch}) {
+export async function load({fetch, params}) {
 
-    const response = await fetch("http://localhost:4000/userData/1");
+    // const response = await fetch("http://localhost:4000/userData/1");
     
+    // const data = await response.json();
+
+    const  response = await fetch(`/api/:1`);
     const data = await response.json();
     
     return {user: data};
--- src/routes/users/[slug]/+page.js
+++ src/routes/users/[slug]/+page.js
@@ -1,10 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({fetch, params}) {
-    const response = await fetch(`http://localhost:4000/userData/${params.slug}`);
-    const  response2 = await fetch(`/api/:${params.slug}`);
-    const data2 = await response2.json();
+    // LOCAL
+    // const response = await fetch(`http://localhost:4000/userData/${params.slug}`);
+    // const data = await response.json();
+
+    const  response = await fetch(`/api/:${params.slug}`);
     const data = await response.json();
-    console.log("DATA", data, "DATA2", data2);
-    return {user: data2};
+    // console.log("DATA", data, "DATA2", data2);
+    return {user: data};
 }

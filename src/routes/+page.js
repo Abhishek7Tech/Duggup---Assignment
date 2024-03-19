497899606c0e2b11a53fe34e35bacd1da5c08773
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-19 19:45:46 +0530

    netlify config updates

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,7 +4,8 @@ export async function load({fetch, params}) {
     
     // const data = await response.json();
 
-    const  response = await fetch(`/api/:1`);
+    // const  response = await fetch(`/api/:1`);
+    const  response = await fetch(`https://duggup-profiles.netlify.app/api/:1`);
     const data = await response.json();
     
     return {user: data};

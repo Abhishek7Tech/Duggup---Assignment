fa622401e533cf0c173d17764d96366a53c423b8
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-20 10:48:04 +0530

    installed vercel adapter

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,11 @@
-export async function load({fetch, params}) {
+// @ts-ignore
+export async function load({fetch}) {
 
     // const response = await fetch("http://localhost:4000/userData/1");
     
     // const data = await response.json();
 
-    // const  response = await fetch(`/api/:1`);
-    const  response = await fetch(`https://duggup-profiles.netlify.app/api/:1`);
+    const  response = await fetch(`/api/:1`);
     const data = await response.json();
     
     return {user: data};
